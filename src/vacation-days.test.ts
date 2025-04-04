3a56f12713edbc31fa78be49f6675837fe9b98aa
simon mettler <lamasion560gmail.com>
2025-04-04 10:54:28 +0200

    Added test for partTime employment (full year)

--- src/vacation-days.test.ts
+++ src/vacation-days.test.ts
@@ -1,18 +1,35 @@
 import { calculateProRataVacationDays, Employment } from "./vacation-days";
 
-test("full year 100% employment gives all vacation days", () => {
-  // Arrange
-  const fullTime: Employment = {
-    startDate: new Date(Date.parse("2025-01-01 00:00:00+01:00")),
-    untilDate: new Date(Date.parse("2025-12-31 23:59:59+01:00")),
-    percentage: 100,
-    vacationDays: 25,
-  };
-  const expected = 25;
 
-  // Act
-  const actual = calculateProRataVacationDays(fullTime);
-
-  // Assert
-  expect(actual).toBe(expected);
+describe("leap year", () => {
+  test("full year 100% employment gives all vacation days", () => {
+    // Arrange
+    const fullTime: Employment = {
+      startDate: new Date(Date.parse("2025-01-01 00:00:00+01:00")),
+      untilDate: new Date(Date.parse("2025-12-31 23:59:59+01:00")),
+      percentage: 100,
+      vacationDays: 25,
+    };
+    const expected = 25;
+  
+    // Act
+    const actual = calculateProRataVacationDays(fullTime);
+  
+    // Assert
+    expect(actual).toBe(expected);
+  });
+  test("full year part time employment for the entire year gives all vacation days", () => {
+    // Arrange
+    const partTime: Employment = {
+      startDate: new Date(Date.parse("2025-01-01 00:00:00+01:00")),
+      untilDate: new Date(Date.parse("2025-12-31 23:59:59+01:00")),
+      percentage: 70,
+      vacationDays: 17.5,
+    };
+    const expected = 17.5 
+    // Act
+    const actual = calculateProRataVacationDays(partTime);
+  
+    // Assert
+    expect(actual).toBe(expected);
 });
