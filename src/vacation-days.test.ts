cc597be847e14e0b1645b556dd55963bfac14814
simon mettler <lamasion560gmail.com>
2025-04-04 11:20:07 +0200

    Added part time employment for part time of the year test

--- src/vacation-days.test.ts
+++ src/vacation-days.test.ts
@@ -48,4 +48,19 @@ describe("leap year", () => {
     // Assert
     expect(actual).toBe(expected);
   });
+  test("part time employment for part time of the year gives all vacation days", () => {
+    // Arrange
+    const partTime: Employment = {
+      startDate: new Date(Date.parse("2025-01-01 00:00:00+01:00")),
+      untilDate: new Date(Date.parse("2025-03-31 23:59:59+01:00")),
+      percentage: 70,
+      vacationDays: 25,
+    };
+    const expected = 4.32 
+    // Act
+    const actual = calculateProRataVacationDays(partTime);
+  
+    // Assert
+    expect(actual).toBe(expected);
+  });
 });
