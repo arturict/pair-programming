e155dc5c334f69d8babaacb3f28a1f03991698eb
Artur Ferreira <artur.ferreira@ckw.ch>
2025-04-14 18:48:02 +0000

    aufgabe 2: erster testfall geschrieben für 16 jähriger mit 700fr lohn

---
+++ src/payroll.test.ts
@@ -0,0 +1,23 @@
+import { calculatePayslip, Salary } from "./payroll"
+
+describe("Lohnabrechnung", () => {
+    test("ein 16 jähriger Lernender mit einem Monatsgehalt von 700.-", () => {
+        //arange
+        const minorSalary: Salary = {
+            born: new Date(Date.parse("2008-05-15 00:00:00+01:00")),
+            payday: new Date(Date.parse("2025-04-25 00:00:00+01:00")),
+            gross: 700
+        };
+        const expected = 687.19;
+        const actual = calculatePayslip(minorSalary);
+
+        //Assert
+        expect(actual).toBe(expected);
+    });
+
+}
+
+
+
+
+)
